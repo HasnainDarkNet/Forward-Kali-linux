cd4ba18e0c1813c784691bfb284c78068c77b281
Hasnain Dark Net <hasnaintiktok304@gmail.com>
2026-06-27 21:23:58 +0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,10 @@
 @echo off
+echo Installing watchdog...
+pip install watchdog >nul 2>&1
+if errorlevel 1 (
+    echo Failed to install watchdog. Installing with --user...
+    pip install watchdog --user
+)
+echo Starting application...
 start /B pythonw "%~dp0window.py"
 exit
